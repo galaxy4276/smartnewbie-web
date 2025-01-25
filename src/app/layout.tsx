78cd60fa1c28197217eccae67568d59bfd258306
galaxy4276 <deveungi@gmail.com>
2025-01-26 02:04:11 +0900

    feat: add open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "스마트뉴비 Careers",
   description: "스마트뉴비 개발팀을 소개합니다!",
+  openGraph: {
+    title: "스마트뉴비 Careers",
+    description: "청년들의 아이디어가 현실이 되는 공간",
+    images: [
+      {
+        url: "/images/og-title.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
